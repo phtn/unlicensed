858f14c160c83e8b746510715cd5b0262b325171
phtn <phtn458@gmail.com>
2026-02-06 23:19:43 +0800

    update:shop-finder

--- components/base44/finder.tsx
+++ components/base44/finder.tsx
@@ -7,7 +7,7 @@ export const ShopFinder = () => {
   const categories = useQuery(api.categories.q.listCategories)
 
   return (
-    <section className='mb-20 border-2 border-zinc-500/40 rounded-3xl pt-3 mt-16 overflow-hidden bg-dark-gray/10'>
+    <section className='mb-20 border-2 border-zinc-500/40 rounded-3xl pt-3 mt-16 overflow-hidden dark:bg-dark-gray/10 bg-dark-gray'>
       <div className='relative max-w-7xl mx-auto'>
         <div className='px-6 md:px-10 py-6 my-2'>
           <h2>
